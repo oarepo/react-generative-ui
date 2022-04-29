38041ee45f597c61817cf40b308555887e230e10
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-04-29 14:12:01 +0200

    fix build of image assets

---
+++ tsdx.config.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 CESNET
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+const images = require('@rollup/plugin-image');
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins = [
+      images({ include: ['**/*.png', '**/*.jpg', '**/*.svg'] }),
+      ...config.plugins,
+    ];
+
+    return config;
+  },
+};
