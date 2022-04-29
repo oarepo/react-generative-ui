80c1b85f55926a4ce91f288ef0b25f34f9f6bdd3
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-04-29 17:55:54 +0200

    cleanup comments

--- src/context/components.tsx
+++ src/context/components.tsx
@@ -18,20 +18,6 @@ import {
 } from '../components';
 import { ComponentMap } from '../types';
 
-// export const defaultComponents = {
-//   authority: (context: UIFragmentContext) => (<Authority {...context} />),
-//   'authority-identifier': (context: UIFragmentContext) => (<AuthorityIdentifier {...context} />),
-//   columns: (context: UIFragmentContext) => (<Columns {...context} />),
-//   column: (context: UIFragmentContext) => (<Column {...context} />),
-//   row: (context: UIFragmentContext) => (<Row {...context} />),
-//   list: (context: UIFragmentContext) => (<List {...context} />),
-//   raw: (context: UIFragmentContext) => (<Raw {...context} />),
-//   'horizontal-list': (context: UIFragmentContext) => (<List {...context} />),
-//   'truncated-text': (context: UIFragmentContext) => (<TruncatedText {...context} />),
-//   _fallback: (context: UIFragmentContext) => (<SemanticFallback {...context} />)
-// } as ComponentMap;
-
-
 export const defaultComponents = {
   authority: Authority,
   'authority-identifier': AuthorityIdentifier,
