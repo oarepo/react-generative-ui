cb86df1f7cbc5650a4b2697dc21b65b65958d8d7
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-05-19 15:56:29 +0200

    update grid column & row stories

--- src/components/structural/Column.stories.tsx
+++ src/components/structural/Column.stories.tsx
@@ -4,6 +4,7 @@ import { Column } from './Column';
 import { UIFragmentContext } from '../../types';
 import 'semantic-ui-css/semantic.min.css'
 import { UIFragment } from '../../GeneratedUI';
+import { Grid } from 'semantic-ui-react';
 
 const meta: Meta = {
   title: 'Structural/Grid/Column',
@@ -16,7 +17,7 @@ const meta: Meta = {
 export default meta;
 
 
-const Template: Story<UIFragmentContext> = (args) => <Column  {...args} />;
+const Template: Story<UIFragmentContext> = (args) => <Grid><Column  {...args} /></Grid>;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
@@ -25,9 +26,9 @@ const config = {
   component: 'column',
   stretched: true,
   items: [
-    { component: 'placeholder' },
-    { component: 'placeholder' },
-    { component: 'placeholder' },
+    { component: 'label', basic: true, content: 'this is item #1' },
+    { component: 'label', basic: true, content: 'this is item #2' },
+    { component: 'label', basic: true, content: 'this is item #3' },
   ]
 }
 
--- src/components/structural/Row.stories.tsx
+++ src/components/structural/Row.stories.tsx
@@ -4,6 +4,7 @@ import { Row } from './Row';
 import { UIFragmentContext } from '../../types';
 import 'semantic-ui-css/semantic.min.css'
 import { UIFragment } from '../../GeneratedUI';
+import { Grid } from 'semantic-ui-react';
 
 const meta: Meta = {
   title: 'Structural/Grid/Row',
@@ -16,13 +17,17 @@ const meta: Meta = {
 export default meta;
 
 
-const Template: Story<UIFragmentContext> = (args) => <Row  {...args} />;
-
+const Template: Story<UIFragmentContext> = (args) => (
+  <Grid>
+    <Row  {...args} />;
+  </Grid>
+)
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
 const config = {
   component: 'row',
+  columnsPerRow: 10,
   columns: [
     { component: 'label', basic: true, content: 'this is item #1' },
     { component: 'label', basic: true, content: 'this is item #2' },
